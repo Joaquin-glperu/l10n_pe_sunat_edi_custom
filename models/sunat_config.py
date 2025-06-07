76707d536d94051ae948b71aef2272bae364e2f1
Joaquin-glperu <bo.wang@glperu.com>
2025-06-06 19:26:06 -0500

    Update sunat_config.py

--- models/sunat_config.py
+++ models/sunat_config.py
@@ -5,10 +5,13 @@ class SunatConfig(models.Model):
     _description = 'SUNAT API Configuration'
 
     name = fields.Char(string='Configuration Name', required=True)
-    sunat_ruc = fields.Char(string='RUC', required=True)
-    sunat_user = fields.Char(string='SOL Username', required=True)
+    sunat_ruc = fields.Char(string='RUC', required=True, default='YOUR_RUC_HERE')
+    sunat_user = fields.Char(string='SOL Username', required=True, default='MODDATOS')
     sunat_password = fields.Char(string='SOL Password', required=True)
-    sunat_api_url = fields.Char(string='API URL', default='https://api.sunat.gob.pe/v1/contribuyente/gem')
+    sunat_api_url = fields.Char(
+        string='API URL',
+        default='https://api.sunat.gob.pe/v1/contribuyente/gem'
+    )
     environment = fields.Selection([
         ('production', 'Production'),
         ('beta', 'Beta / Test'),
